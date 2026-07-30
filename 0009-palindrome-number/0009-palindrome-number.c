43c80f862e2c3eec0d1bcebb4c52e0578e64928d
Anish Das <anishdas1410@gmail.com>
2026-07-30 14:14:26 +0530

    Time: 4 ms (24.86%), Space: 8.9 MB (65.14%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,14 @@
+bool isPalindrome(int x) {
+    double rev=0;
+    double rem=0;
+    int num=x;
+    if(x<0){
+        return false;
+    }
+    while(x!=0){
+        rem =x%10;
+        rev=rev*10+rem;
+        x=x/10;
+    }
+    return rev==num;
+}
