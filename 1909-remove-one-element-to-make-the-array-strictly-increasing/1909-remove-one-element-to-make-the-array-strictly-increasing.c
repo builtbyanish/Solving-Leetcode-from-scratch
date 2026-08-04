49a51e53e277ab4774d1d0d85d1794ac4a056a8c
Anish Das <anishdas1410@gmail.com>
2026-08-04 22:00:26 +0530

    Time: 0 ms (100%), Space: 9.1 MB (35.53%) - LeetHub

---
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.c
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+
+bool canBeIncreasing(int* nums, int numsSize) {
+    int count = 0;
+
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (nums[i] >= nums[i + 1]) {
+            count++;
+
+            if (count > 1)
+                return false;
+
+            if (i > 0 && nums[i - 1] >= nums[i + 1]) {
+                nums[i + 1] = nums[i];
+            }
+        }
+    }
+
+    return true;
+}
