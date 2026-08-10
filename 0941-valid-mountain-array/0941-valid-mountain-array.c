355ab8c854ee90f3c2571cdd26a9ed87e82a1bfa
Anish Das <anishdas1410@gmail.com>
2026-08-10 16:53:00 +0530

    Time: 21 ms (35.3%), Space: 9.8 MB (97.27%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.c
@@ -0,0 +1,18 @@
+bool validMountainArray(int* arr, int arrSize)
+{
+    if (arrSize < 3)
+        return false;
+
+    int i = 0;
+
+    while (i + 1 < arrSize && arr[i] < arr[i + 1])
+        i++;
+
+    if (i == 0 || i == arrSize - 1)
+        return false;
+
+    while (i + 1 < arrSize && arr[i] > arr[i + 1])
+        i++;
+
+    return i == arrSize - 1;
+}
